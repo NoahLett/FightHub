9c8818303f0d22b0d100fc3210a83dbdffd9b959
Noah Lett <noahblett96@gmail.com>
2024-03-07 14:21:08 -0800

    Set up initial server

---
+++ server/routes/auth.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+router.post('/register', async (req, res) => {
+    console.log('Test');
+})
+
+module.exports = router;
